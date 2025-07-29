97571051a0ca53c51f6061aebc03f42bb5d4e97a
H S <2413655@cognizant.com>
2025-07-29 11:07:43 +0530

    29

--- git-session-YHS/src/main/java/com/example/git/App.java
+++ git-session-YHS/src/main/java/com/example/git/App.java
@@ -2,6 +2,6 @@ package main.java.com.example.git;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("GITHUB SESSION BY Shree Khaanth -- git newBranchm");
+        System.out.println("GITHUB SESSION fordemo branch");
     }
 }
