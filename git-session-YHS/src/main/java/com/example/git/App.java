fc507e04c164685995667ff84523fe111e943436
H S <2413655@cognizant.com>
2025-07-29 11:43:38 +0530

    29 chnages

--- git-session-YHS/src/main/java/com/example/git/App.java
+++ git-session-YHS/src/main/java/com/example/git/App.java
@@ -2,6 +2,6 @@ package main.java.com.example.git;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("GITHUB SESSION fordemo branch");
+        System.out.println("GITHUB SESSION on 29th july");
     }
 }
